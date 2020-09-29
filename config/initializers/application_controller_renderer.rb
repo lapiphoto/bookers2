6c36e199a777b4de532da3c2d94d7f9e875a01e2
lapiphoto <69066821+lapiphoto@users.noreply.github.com>
2020-09-29 22:43:39 +0900

    Initial Commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '3203ab57ec3de7afe3995f2330d13997febe0494fd9616771a01192b4a92b6890ad591bd1632b0fccadcc33a037f60b316cf5910d640b2e1e71bd37e20419ce9'
